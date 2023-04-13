60d6b6415980cbbe4f456e37f5e5191b97bd12e2
David Lee <dtlee@bu.edu>
2023-04-13 00:38:09 -0400

    Delete spotify.js

--- prototype/config/spotify.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  spotify: {
-    PORT: 5000,
-    CLIENT_ID: "06c7e114385248db8d97898f5da4ba21",
-    CLIENT_SECRET: "27a50a94b0bb47d3ac26acdb17af3a62",
-    REDIRECTURI: "http://localhost:5000/api/logged",
-    CLIENT_REDIRECTURI: "http://localhost:3000/"
-  }
-};
